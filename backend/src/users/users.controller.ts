1684a1a4319ac66eb42dfeadeba27b9560e84d84
akhvarastyan-web <a.khvarastyan@gmail.com>
2026-04-29 21:25:24 +0300

    Create a get profile endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { User } from './user.entity';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+
+interface RequestWithUser {
+  user: User;
+}
 
 @Controller('auth')
 export class UsersController {
@@ -15,5 +29,13 @@ export class UsersController {
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.usersService.login(loginDto);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiOperation({ summary: 'Get current user profile by token' })
+  getProfile(@Request() req: RequestWithUser): User {
+    return req.user;
   }
 }
